3b76816265c5e2760a40404b40139fc3e85fbc1a
siaochanwu <jill007tw@gmail.com>
2021-09-15 13:39:38 +0800

    fix: cors setting

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 function verify(token) {
     // 把 token 送到後端
-    // var formData = new FormData();
-    // formData.append('token', token);
+    var formData = new FormData();
+    formData.append('token', token);
         
     // Google Apps Script 部署為網路應用程式後取得的 URL
-    var uriGAS = `https://script.google.com/macros/s/AKfycbxJOgOXrxs08GlO3vNxCE69BC2zjtmWeAjlOfGp7HqSWezkzbp80mis8UmLCBkqR-gA/exec?url=https://www.google.com/recaptcha/api/siteverify&token=${token}`;
+    var uriGAS = "https://script.google.com/macros/s/AKfycbxJOgOXrxs08GlO3vNxCE69BC2zjtmWeAjlOfGp7HqSWezkzbp80mis8UmLCBkqR-gA/exec";
         
     fetch(uriGAS, {
-        method: "GET",
-        header: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Access-Control-Allow-Origin": "*",
+        redirect: "follow",
+        method: "POST",
+        headers: {
+            'Access-Control-Allow-Origin': '*'
         },
+        body: formData,
     }).then(response => response.json())
         .then(result => {
         if(result.success) {
